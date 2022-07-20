c0bafec0953feec100ab708a0149c7638c157644
Poulami807 <poulamiganguly10@gmail.com>
2022-07-20 13:44:23 +0530

    completed by Poulami

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-//Enter your code here..
+async function contactInfo(){
+    try {
+       let res = await fetch('https://jsonplaceholder.typicode.com/users')
+       let res2 = await res.json()
+       console.log(res2)
+       var player='<h2>Lists of Users</h2>'
+       res2.forEach(user => {
+            player +=
+           `<div class="player">
+            <div class="strength">Name : ${user.name}</div>
+            <div>Email   : ${user.email}</div>
+            <div>Phone   : ${user.phone}</div>
+            <div>Website : ${user.website}</div>
+            <div>Company : ${user.company.name}</div>
+            <div>City    : ${user.address.city}</div>
+            <div>Zipcode : ${user.address.zipcode}</div>
+            </div>`
+       })
+       document.getElementById('message').innerHTML = player  
+    } 
+    catch (error) {
+        console.log('Promise rejected.');
+        console.log(error.message);
+    }
+   }
